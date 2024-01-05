5bcbb7d56d462781e598d80b103e3c283313ad92
Kavinda <kavindasamarawickkrama2001@gmail.com>
2024-01-06 01:54:38 +0530

    chng

---
+++ lms-frontend/src/pages/faculties/index.js
@@ -0,0 +1,192 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import Head from 'next/head';
+import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
+import ArrowPathIcon from '@heroicons/react/24/solid/ArrowPathIcon';
+import { Box, Button, Container, LinearProgress, Stack, SvgIcon, Typography } from '@mui/material';
+import { useSelection } from 'src/hooks/use-selection';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import { TrucksTable } from 'src/sections/trucks/trucks-table';
+import { BigSearch } from 'src/sections/big-search';
+import { applyPagination } from 'src/utils/apply-pagination';
+import NextLink from 'next/link';
+import { StyledBreadCrumbs } from 'src/components/breadcrumbs';
+import { useRouter } from 'next/navigation';
+import { useConfirm } from 'material-ui-confirm';
+import { getAllTrucks } from 'src/apis/trucks';
+import { searchObjects } from 'src/utils/search-objects';
+import { useSnackbar } from 'notistack';
+
+
+const useTrucks = (data, page, rowsPerPage, search) => {
+  return useMemo(
+    () => {
+      const filtered = searchObjects(data, search)
+      return applyPagination(filtered, page, rowsPerPage);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [page, rowsPerPage, data, search]
+  );
+};
+
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const trucks = useTrucks(data, page, rowsPerPage, search);
+
+  const [loading, setLoading] = useState(true)
+
+  const { enqueueSnackbar } = useSnackbar()
+  async function retrieveAndRefreshData() {
+    setLoading(true)
+    try {
+      const trucks = (await getAllTrucks()) || [];
+      console.log("Trucks were fetched from the database", trucks)
+
+      setData(trucks)
+    } catch (e) {
+      enqueueSnackbar('Error occured!', {
+        variant: 'error',
+        anchorOrigin: {
+          vertical: 'bottom',
+          horizontal: 'right',
+
+        },
+        autoHideDuration: 2000
+      })
+      console.error(e)
+    }
+    setLoading(false)
+  }
+
+
+  useEffect(() => {
+    retrieveAndRefreshData()
+  }, [])
+
+  const handlePageChange = useCallback(
+    (event, value) => {
+      setPage(value);
+    },
+    []
+  );
+
+  const handleRowsPerPageChange = useCallback(
+    (event) => {
+      setRowsPerPage(event.target.value);
+    },
+    []
+  );
+
+  const confirm = useConfirm()
+
+  const handleDelete = (truck) => {
+    confirm({ description: `This will permanently delete the record` })
+      .then(() => {
+        // TODO: Delete the data, api call
+
+        retrieveAndRefreshData()
+      })
+      .catch(() => {
+        enqueueSnackbar('Error occured', {
+          variant: 'error',
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'right',
+
+          },
+          autoHideDuration: 2000
+        })
+        console.log("Deletion cancelled.")
+      });
+  };
+
+  return(
+
+    <>
+      <Head>
+        <title>
+          Faculties
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 4
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              spacing={4}
+            >
+              <Stack spacing={1}>
+                <Typography variant="h5">
+                  Faculties
+                </Typography>
+
+                <StyledBreadCrumbs sequence={[
+                  {
+                    text: 'faculties',
+                    linkUrl: '/faculties',
+                    active: true
+                  },
+                ]} />
+
+              </Stack>
+              <div>
+                <Stack
+                  spacing={1}
+                  direction={'row'}
+                >
+
+                  <Button
+                    startIcon={(
+                      <SvgIcon fontSize="small">
+                        <ArrowPathIcon />
+                      </SvgIcon>
+                    )}
+                    onClick={() => retrieveAndRefreshData()}
+                    variant="outlined"
+                  >
+                    Refresh
+                  </Button>
+                </Stack>
+              </div>
+            </Stack>
+            <BigSearch
+              search={search}
+              onSearch={setSearch}
+              placeholder={"Search faculties"}
+            />
+
+            {loading && <LinearProgress />}
+
+            <TrucksTable
+              count={data.length}
+              items={trucks}
+              onPageChange={handlePageChange}
+              onRowsPerPageChange={handleRowsPerPageChange}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              handleDelete={handleDelete}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Page;
